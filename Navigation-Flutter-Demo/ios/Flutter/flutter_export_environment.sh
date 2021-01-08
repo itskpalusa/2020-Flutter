7c78d1112f02e3e7c6d1bac5c7c8a9f63b2c3a0f
Karthik Palusa <karthik.palusa@gmail.com>
2021-01-07 21:37:08 -0700

    BMI Calculator App

--- Navigation-Flutter-Demo/ios/Flutter/flutter_export_environment.sh
+++ Navigation-Flutter-Demo/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/karthikpalusa/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/karthikpalusa/Projects/AndroidStudioProjects/2020-Flutter/bmi-calculator-flutter"
-export "FLUTTER_TARGET=/Users/karthikpalusa/Projects/AndroidStudioProjects/2020-Flutter/bmi-calculator-flutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/karthikpalusa/Projects/AndroidStudioProjects/2020-Flutter/Navigation-Flutter-Demo"
+export "FLUTTER_TARGET=/Users/karthikpalusa/Projects/AndroidStudioProjects/2020-Flutter/Navigation-Flutter-Demo/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
---
+++ bmi-calculator-flutter/lib/components/icon_content.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class IconContent extends StatelessWidget {
+  IconContent({this.icon, this.text});
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(height: 50),
+        Text(text, style: kLabelTextStyle),
+      ],
+    );
+  }
+}
